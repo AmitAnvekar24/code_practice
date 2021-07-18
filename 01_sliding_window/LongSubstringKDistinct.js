e0fc99c4d2a3f53b85a6084475747052fafac6d8
AmitAnvekar24 <anvekaramit24@gmail.com>
2021-07-19 02:20:29 +0530

    Longest substring with no more than K distinct chars.

---
+++ 01_sliding_window/LongSubstringKDistinct.js
@@ -0,0 +1,45 @@
+/*
+    Problem Statement #
+    Given a string, find the length of the longest substring in it with no more 
+    than K distinct characters.
+*/
+
+// Complexity --> O( N + N ) --> O(N) --> Optimal solution
+function LongSubstringKDistinct( str, K ) {    
+    let charFrequency = {};
+    let ans = 0, diff = 0, startWindow = 0;
+
+    for (let index = 0; index < str.length; index++) {
+        let char = str.charAt(index);
+        
+        if(charFrequency.hasOwnProperty(char)){
+            charFrequency[char] = charFrequency[char] + 1;
+        }
+        else{
+            charFrequency[char] = 1;
+        }
+
+        if( Object.keys(charFrequency).length > K ){
+            diff = index - startWindow;
+            while( Object.keys(charFrequency).length > K ){   
+                let tempChar = str.charAt(startWindow);
+                if( charFrequency[tempChar] > 1 ){
+                    charFrequency[tempChar] = charFrequency[tempChar] - 1;  
+                }else{
+                    delete charFrequency[tempChar];
+                }
+                startWindow++;
+            }
+            if( diff > ans ){
+                ans  = diff;
+            }
+        }
+        
+    }
+    return ans;
+}
+
+
+console.log(LongSubstringKDistinct("araaci",2));
+console.log(LongSubstringKDistinct("araaci",1));
+console.log(LongSubstringKDistinct("cbbebi",3));
